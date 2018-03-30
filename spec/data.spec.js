83accf493e20002eb18416abdb0cad6c2e1802cc
Anthony Chu <anthony@anthonychu.ca>
2018-03-29 23:23:24 -0700

    Update data.spec.js

--- spec/data.spec.js
+++ spec/data.spec.js
@@ -4,6 +4,6 @@ const data = require('../lib/data');
 describe('Current city', function() {
   it('should be correct', function() {
     const actual = data.currentCity();
-    assert.equal(actual, 'World');
+    assert.equal(actual, 'Vancouver');
   });
 });
